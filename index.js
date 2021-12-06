57aa4f1fa4057d14f1d1f984a3c4f62fc87e1b84
Tony Huang <tonyhuang@Tonys-MacBook-Pro.local>
2021-12-06 18:17:34 -0500

    finish function

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb+srv://tonyhuang12138:Tony123456@cluster0.ypk43.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";  // connection string goes here
 
-  MongoClient.connect(url, { useUnifiedTopology: true }, function(err, db) {
-  if(err) { return console.log(err); return;}
-  
-    var dbo = db.db("database");
-	var collection = dbo.collection('companies');
-	
-	console.log("Success!");
-	db.close();
- 
+// Establish connection
+MongoClient.connect(url, { useUnifiedTopology: true }, function (err, db) {
+  if (err) { return console.log(err); }
+
+  var dbo = db.db("database");
+  var collection = dbo.collection('companies');
+
+  // Read line
+  const fs = require('fs');
+  const readline = require('readline');
+  const file = readline.createInterface({
+    input: fs.createReadStream('./companies.csv'),
+  });
+
+  let i = 0;
+  file.on('line', (line) => {
+    if (i != 0) {
+      substring = line.split(",");
+      var newData = { Company: substring[0], Ticker: substring[1]};
+      collection.insertOne(newData, function (err, res) {
+        if (err) { console.log("query err: " + err); return; }
+        console.log("new document inserted");
+      });
+
+    }
+    i++;
+  });
 });
+
+
+
 
 
